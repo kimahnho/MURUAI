c679391b2d78e00befde74ca9e928c40f63ee5c0
oriNuguri25 <byeongjun25@naver.com>
2026-01-29 21:50:45 +0900

    feat: 에러 수정

--- src/features/editor/ui/parts/template_component/text/hooks/useTextBoxSelectionEffect.ts
+++ src/features/editor/ui/parts/template_component/text/hooks/useTextBoxSelectionEffect.ts
@@ -52,6 +52,8 @@ export const useTextBoxSelectionEffect = ({
       });
     });
 
-    return () => { cancelAnimationFrame(frame); };
+    return () => {
+      cancelAnimationFrame(frame);
+    };
   }, [isEditing, editableRef, pendingCaretRef, pendingWordSelectRef]);
 };
